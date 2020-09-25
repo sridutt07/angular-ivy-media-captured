445ea55182116ad94f394d5608e917f1d6666255
sridutt07 <ksridutt0@gmail.com>
2020-09-25 13:29:25 +0530

    3 files modified

--- src/app/media-capture/components/video-capture/video-capture.component.ts
+++ src/app/media-capture/components/video-capture/video-capture.component.ts
@@ -1,34 +1,26 @@
-import {
-  Component,
-  ViewChild,
-  ElementRef,
-  AfterViewInit,
-  Output,
-  EventEmitter,
-  Input,
-  OnDestroy,
-  ChangeDetectorRef
-} from "@angular/core";
-import { DomSanitizer } from "@angular/platform-browser";
-import RecordRTC from "recordrtc";
+import { Component, ViewChild, ElementRef, AfterViewInit, Output, EventEmitter, Input, OnDestroy, ChangeDetectorRef } from '@angular/core';
+import { DomSanitizer } from '@angular/platform-browser';
+import * as RecordRTC from 'recordrtc';
 
-import { MediaType } from "../../enums/media-type.enum";
+import { MediaType } from '../../enums/media-type.enum';
 
-import { MediaModel } from "../../model/media.model";
-import { MediaInputModel } from "../../model/media-input.model";
-import { mediaConstants } from "../../constants";
+import { MediaModel } from '../../model/media.model';
+import { MediaInputModel } from '../../model/media-input.model';
+import { mediaConstants } from '../../constants';
+
 
 const recordRtc = RecordRTC;
 
+
 @Component({
-  selector: "lib-video-capture",
-  templateUrl: "./video-capture.component.html",
-  styleUrls: ["./video-capture.component.scss"]
+  selector: 'lib-video-capture',
+  templateUrl: './video-capture.component.html',
+  styleUrls: ['./video-capture.component.scss']
 })
 export class VideoCaptureComponent implements AfterViewInit, OnDestroy {
   @Input() mediaInputData: MediaInputModel;
   @Output() mediaCaptured = new EventEmitter<MediaModel>();
-  @ViewChild("video", { static: false }) video: ElementRef;
+  @ViewChild('video', { static: false }) video: ElementRef;
 
   private readonly mediaData = new MediaModel();
   private stream: MediaStream;
@@ -39,18 +31,21 @@ export class VideoCaptureComponent implements AfterViewInit, OnDestroy {
   private readonly intervalPeriod = 100;
   private readonly msPerSec = 1000;
 
+
   browser = navigator as any;
   currentVideoSize = 0;
   isCaptured = false;
   isRecording = false;
   pauseHandler = false;
-  currentTime = "00:00";
+  currentTime = '00:00';
   isPaused = false;
   maxTimeDuration: string;
   mediaType: string;
-  errorMessage = "";
+  errorMessage = '';
   isRearCameraPresent = false;
-  facingMode = "user";
+  facingMode = 'user';
+  videoAngle = 0;
+
 
   constraints = {
     video: {
@@ -61,21 +56,20 @@ export class VideoCaptureComponent implements AfterViewInit, OnDestroy {
 
   constructor(
     private readonly sanitizer: DomSanitizer,
-    private readonly changeDetectorRef: ChangeDetectorRef
+    private readonly changeDetectorRef: ChangeDetectorRef,
   ) {
-    this.browser.getUserMedia =
-      this.browser.getUserMedia ||
+    this.browser.getUserMedia = (this.browser.getUserMedia ||
       this.browser.webkitGetUserMedia ||
       this.browser.mozGetUserMedia ||
-      this.browser.msGetUserMedia;
+      this.browser.msGetUserMedia);
   }
 
   async ngAfterViewInit() {
     const devices = await navigator.mediaDevices.enumerateDevices();
-    const videoDevices = devices.filter(device => device.kind === "videoinput");
+    const videoDevices = devices.filter((device) => device.kind === 'videoinput');
     if (videoDevices.length > 1) {
       this.isRearCameraPresent = true;
-      this.facingMode = "environment";
+      this.facingMode = 'environment';
       this.constraints = {
         video: {
           facingMode: this.facingMode
@@ -88,26 +82,22 @@ export class VideoCaptureComponent implements AfterViewInit, OnDestroy {
     this.videoViewer.muted = true;
     this.videoViewer.controls = false;
     this.initVideo();
-    this.maxTimeDuration = "20";
+    this.maxTimeDuration = "20"
   }
 
   initVideo() {
     // [TOUPGRADE] reduce the time it takes for cam to initialize in all three captures
     this.videoViewer.autoplay = true;
-    navigator.mediaDevices
-      .getUserMedia(this.constraints)
-      .then(async (currentStream: MediaStream) => {
-        this.stream = currentStream;
-        if (!this.changeDetectorRef["destroyed"]) {
-          this.recordRtc = new recordRtc(currentStream,{
-type: mediaConstants.videoMimeTypes.mkv
-});
-          this.recordRtc.startRecording();
-          this.videoViewer.srcObject = currentStream;
-        } else {
-          this.closeStream();
-        }
-      });
+    navigator.mediaDevices.getUserMedia(this.constraints).then(async (currentStream: MediaStream) => {
+      this.stream = currentStream;
+      if (!this.changeDetectorRef['destroyed']) {
+        this.recordRtc = recordRtc(currentStream, { type: mediaConstants.videoMimeTypes.mkv });
+        this.recordRtc.startRecording();
+        this.videoViewer.srcObject = currentStream;
+      } else {
+        this.closeStream();
+      }
+    })
   }
 
   toggleControls() {
@@ -120,11 +110,7 @@ type: mediaConstants.videoMimeTypes.mkv
     if (this.stream.active) {
       this.recordRtc.stopRecording(() => {
         this.videoViewer.srcObject = this.videoViewer.src = null;
-        this.videoViewer.src = URL.createObjectURL(
-          new Blob([this.recordRtc.getBlob()], {
-            type: mediaConstants.videoMimeTypes.mkv
-          })
-        );
+        this.videoViewer.src = URL.createObjectURL(new Blob([this.recordRtc.getBlob()], { type: mediaConstants.videoMimeTypes.mkv }));
       });
       this.stream.getAudioTracks().forEach(track => track.stop());
       this.stream.getVideoTracks().forEach(track => track.stop());
@@ -137,40 +123,27 @@ type: mediaConstants.videoMimeTypes.mkv
       timeSlice: mediaConstants.timeSlice
     };
 
-    navigator.mediaDevices
-      .getUserMedia(this.constraints)
-      .then((currentStream: MediaStream) => {
-        this.stream = currentStream;
-        this.recordRtc = new recordRtc(currentStream, options);
-        this.videoViewer.srcObject = currentStream;
-        this.isRecording = true;
-        this.recordRtc
-          .setRecordingDuration(this.mediaInputData.videoDuration)
-          .onRecordingStopped(this.stopRecording);
-        this.currentTime = "00:00";
-        this.recordRtc.startRecording();
-        this.sizeIntervalHandler = setInterval(
-          this.checkVideoSize,
-          this.intervalPeriod
-        );
-      });
+    navigator.mediaDevices.getUserMedia(this.constraints).then((currentStream: MediaStream) => {
+      this.stream = currentStream;
+      this.recordRtc = recordRtc(currentStream, options);
+      this.videoViewer.srcObject = currentStream;
+      this.isRecording = true;
+      this.recordRtc.setRecordingDuration(this.mediaInputData.videoDuration).onRecordingStopped(this.stopRecording);
+      this.currentTime = '00:00';
+      this.recordRtc.startRecording();
+      this.sizeIntervalHandler = setInterval(this.checkVideoSize, this.intervalPeriod);
+    });
   }
 
-  checkVideoSize = () => {
-    // required to get same instance of recordRtc
+  checkVideoSize = () => { // required to get same instance of recordRtc
     if (!this.recordRtc) {
       return;
     }
     const recorder = this.recordRtc.getInternalRecorder();
     if (recorder && recorder.getArrayOfBlobs) {
-      const blob = new Blob(recorder.getArrayOfBlobs(), {
-        type: mediaConstants.videoMimeTypes.mkv
-      });
+      const blob = new Blob(recorder.getArrayOfBlobs(), { type: mediaConstants.videoMimeTypes.mkv });
       this.currentVideoSize = blob.size;
-      if (
-        blob.size >= this.mediaInputData.videoSize ||
-        this.progressBarTime > this.mediaInputData.videoDuration
-      ) {
+      if (blob.size >= this.mediaInputData.videoSize || this.progressBarTime > this.mediaInputData.videoDuration) {
         // [TOUPGRADE] show toast when stopped due to exceeding video size
         this.recordRtc.stopRecording(this.stopRecording);
         return;
@@ -179,10 +152,9 @@ type: mediaConstants.videoMimeTypes.mkv
         this.currentTime = "33";
       }
     }
-  };
+  }
 
-  stopRecording = () => {
-    // required to get same instance of recordRtc
+  stopRecording = () => { // required to get same instance of recordRtc
     this.recordRtc.stopRecording(() => {
       this.isRecording = false;
       this.videoViewer.autoplay = false;
@@ -191,9 +163,7 @@ type: mediaConstants.videoMimeTypes.mkv
       this.mediaData.file = this.recordRtc.getBlob();
       this.mediaData.mimeType = mediaConstants.videoMimeTypes.mkv;
       this.mediaData.fileTypeId = MediaType.Video;
-      this.mediaData.fileUrl = this.sanitizer.bypassSecurityTrustUrl(
-        URL.createObjectURL(this.recordRtc.getBlob())
-      );
+      this.mediaData.fileUrl = this.sanitizer.bypassSecurityTrustUrl(URL.createObjectURL(this.recordRtc.getBlob()));
       this.videoViewer.src = URL.createObjectURL(this.mediaData.file);
       // [HACK] To remove after chrome bug is fixed
 
@@ -203,15 +173,28 @@ type: mediaConstants.videoMimeTypes.mkv
     });
     this.stream.getAudioTracks().forEach(track => track.stop());
     this.stream.getVideoTracks().forEach(track => track.stop());
-  };
+  }
 
   confirm() {
     this.mediaCaptured.emit(this.mediaData);
   }
 
+  rotateCamera(direction: string) {
+    if (direction === 'left') {
+      this.videoAngle = this.videoAngle - 90;
+    } else {
+      this.videoAngle = this.videoAngle + 90;
+    }
+
+    if (this.videoAngle === 360 || this.videoAngle === -360) {
+      this.videoAngle = 0;
+    }
+
+  }
+
   flipCamera() {
     this.stream.getVideoTracks().forEach(track => track.stop());
-    this.facingMode = this.facingMode === "user" ? "environment" : "user";
+    this.facingMode = this.facingMode === 'user' ? 'environment' : 'user';
     this.constraints = {
       video: {
         facingMode: this.facingMode
@@ -241,10 +224,7 @@ type: mediaConstants.videoMimeTypes.mkv
       clearInterval(this.sizeIntervalHandler);
       this.recordRtc.pauseRecording();
     } else {
-      this.sizeIntervalHandler = setInterval(
-        this.checkVideoSize,
-        this.intervalPeriod
-      );
+      this.sizeIntervalHandler = setInterval(this.checkVideoSize, this.intervalPeriod);
       this.recordRtc.resumeRecording();
     }
     setTimeout(() => {
@@ -253,7 +233,6 @@ type: mediaConstants.videoMimeTypes.mkv
   }
 
   private closeStream() {
-    debugger;
     if (this.stream !== undefined && this.stream.active) {
       this.stream.getVideoTracks().forEach(track => track.stop());
       this.stream.getAudioTracks().forEach(track => track.stop());
@@ -262,15 +241,14 @@ type: mediaConstants.videoMimeTypes.mkv
   }
 
   ngOnDestroy() {
-    debugger;
     this.closeStream();
   }
 
   getBlobDuration(blob): Promise<number> {
-    const tempVideoEl = document.createElement("video");
+    const tempVideoEl = document.createElement('video');
 
     const durationP = new Promise<number>(resolve =>
-      tempVideoEl.addEventListener("loadedmetadata", () => {
+      tempVideoEl.addEventListener('loadedmetadata', () => {
         if (tempVideoEl.duration === Infinity) {
           tempVideoEl.currentTime = Number.MAX_SAFE_INTEGER;
           tempVideoEl.ontimeupdate = () => {
@@ -281,7 +259,7 @@ type: mediaConstants.videoMimeTypes.mkv
         } else {
           resolve(tempVideoEl.duration);
         }
-      })
+      }),
     );
 
     tempVideoEl.src = window.URL.createObjectURL(blob);
